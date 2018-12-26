fbc9b6410d091b44c698cc37a38482ea8dc78a2f
Yerko Henriquez <yerko.anton.roj@hotmail.com>
2018-12-26 11:53:18 -0300

    redux -> react-redux

--- src/redux/ducks/index.js
+++ src/redux/ducks/index.js
@@ -1,4 +1,4 @@
-import { combineReducers } from "redux";
+import { combineReducers } from "react-redux";
 import fetchAnime from "./fetchAnime";
 import modes from "./modes";
 import sideBar from "./sideBar";
