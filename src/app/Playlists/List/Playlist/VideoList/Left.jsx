45ed93f1e1c1805dca170f4668af63b975ee791a
Waimog <yerko.anton.roj@hotmail.com>
2019-03-07 01:56:30 -0300

    small section draggable deleted

--- src/app/Playlists/List/Playlist/VideoList/Left.jsx
+++ src/app/Playlists/List/Playlist/VideoList/Left.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import { MoreVertical } from "react-feather";
 import { VideoLeft } from "./styles/leftStyles";
 
 const Left = ({ index, title }) => (
   <VideoLeft>
-    <MoreVertical className="draggable" />
     <span>{index + 1}</span>
     <div className="pl-video-name">{title.substring(0, 50) + "..."}</div>
   </VideoLeft>
--- src/app/Playlists/List/Playlist/VideoList/styles/leftStyles.js
+++ src/app/Playlists/List/Playlist/VideoList/styles/leftStyles.js
@@ -3,22 +3,12 @@ import styled from "styled-components";
 export const VideoLeft = styled.div`
   display: flex;
   flex-direction: row;
-  position: relative;
-  width: 100%;
-  height: 100%;
   align-items: center;
-  .draggable {
-    visibility: hidden;
-    position: absolute;
-    left: 0;
-    width: 15px;
-    height: 100%;
-    cursor: move;
-  }
   span {
-    margin: 0 1.5rem;
+    margin: 0 1rem;
   }
   .pl-video-name {
-    width: 300px;
+    margin-left: 1rem;
+    width: 340px;
   }
 `;
